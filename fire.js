0c18d2e76d346b6b3b4c9e52ed52526ddfe501db
Surya Venkatesan <suryaedu71@gmail.com>
2024-03-31 12:11:34 +0530

    Dummy push to trigger user name updation

--- fire.js
+++ fire.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
 // import { getAuth, createUserWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js'
 import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js'
 
